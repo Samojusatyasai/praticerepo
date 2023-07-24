5acb3ac43c5917389a5ec4209e41cfb976b224f3
satyasai.samoju <satyasai.samoju@INBL01LAP31245.brd.g-c-i.com>
2023-07-24 21:34:58 +0530

    New Commit

---
+++ Test Suites/New Test Suite1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b84d556f-7b95-4106-8ebf-606daa3233db</testSuiteGuid>
+</TestSuiteEntity>
